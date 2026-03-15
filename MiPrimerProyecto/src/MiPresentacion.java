d31f81aecdcd195a52c361cfafd5a6ed7dba3848
FernandoVilla11 <jose.villadiego096@gmail.com>
2026-03-14 23:45:55 -0500

    first commit

---
+++ MiPrimerProyecto/src/MiPresentacion.java
@@ -0,0 +1,23 @@
+public class MiPresentacion {
+//Tu nombre completo
+// Tu edad actual
+// La ciudad donde vives
+// Un mensaje personalizado de presentación
+
+    public static void main(String[] args) {
+
+        String nombre = "Jose Albornoz";
+        int edad = 29;
+        String ciudad = "Medellin";
+        String mensaje = "Este es mi primer proyecto";
+
+        System.out.println("Hola, mi nombre es " + nombre);
+        System.out.println("Tengo " + edad + " años");
+        System.out.println("Vivo en " + ciudad);
+        System.out.println(mensaje);
+        
+    }
+
+    
+
+}
